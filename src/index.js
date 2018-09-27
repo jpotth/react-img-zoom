c78d89084a73930bead997d1f7e4e152a64f6054
Jordan Potthoff <jordan.potth@gmail.com>
2018-09-26 18:34:45 -0700

    on image load

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import React, { Component, createRef } from 'react'
 import PropTypes from 'prop-types'
 import styles from './style.css'
 
+const ZOOM_EVENT_TYPES = {
+  HOVER: "hover",
+  CLICK: "click"
+};
+
 class Zoom extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
       zoom: false,
+      zoomEvent: ZOOM_EVENT_TYPES.HOVER,
       mouseX: null,
       mouseY: null,
+      imageLoaded: false
     }
 
     const {
       height,
-      img,
       transitionTime,
       width,
     } = props
@@ -28,11 +34,7 @@ class Zoom extends Component {
 
     this.innerDivStyle = {
       height: `${height}px`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundSize: 'auto 100%',
       transition: `transform ${transitionTime}s ease-out`,
-      backgroundImage: `url('${img}')`,
     }
 
     this.imageRef = createRef()
@@ -40,6 +42,7 @@ class Zoom extends Component {
     this.handleMouseOver = this.handleMouseOver.bind(this)
     this.handleMouseOut = this.handleMouseOut.bind(this)
     this.handleMouseMovement = this.handleMouseMovement.bind(this)
+    this.handleOnImageLoad = this.handleOnImageLoad.bind(this)
   }
 
   handleMouseOver () {
@@ -78,6 +81,18 @@ class Zoom extends Component {
     })
   }
 
+  handleOnImageLoad() {
+    this.setState{(imageLoaded: true});
+  }
+
+  renderLoadingDiv() {
+    if(this.props.loadingDiv) {
+      return this.props.loadingDiv;
+    } else {
+      return <div />;
+    }
+  }
+
   render () {
     const {
       mouseX,
@@ -86,6 +101,7 @@ class Zoom extends Component {
     } = this.state
 
     const {
+      img,
       zoomScale,
     } = this.props
 
@@ -101,13 +117,16 @@ class Zoom extends Component {
         onMouseMove={this.handleMouseMovement}
         ref={this.imageRef}
       >
-        <div
+        {!this.state.imageLoaded && this.renderLoadingDiv()}
+        <img
+          src={img}
           style={{
             ...transform,
             ...this.innerDivStyle,
             transform: zoom ? `scale(${zoomScale})` : 'scale(1.0)',
           }}
           className={styles.zoomImg}
+          onLoad={this.props.onLoad}
         />
       </div>
     )
@@ -119,6 +138,10 @@ Zoom.propTypes = {
   img: PropTypes.string.isRequired,
   /** The zoom scale. */
   zoomScale: PropTypes.number.isRequired,
+  /** The event that triggers a zoom effet. */
+  zoomEvent: PropTypes.string,
+  /** The maximum zoom scale. */
+  maxZoomScale: PropTypes.number,
   /** The height of the image in pixels */
   height: PropTypes.number.isRequired,
   /** The width of the image in pixels */
@@ -128,7 +151,9 @@ Zoom.propTypes = {
 }
 
 Zoom.defaultProps = {
-  transitionTime: 0.1,
+  zoomEvent: "hover",
+  maxZoomScale: 2,
+  transitionTime: 0.1
 }
 
 export default Zoom
